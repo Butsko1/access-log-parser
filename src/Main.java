79a8dba654375eeb7d09beb76be8a5aa731d90dd
ASButsko <ASButsko@inno.tech>
2025-05-14 17:09:44 +0300

    Добавлен метод, принимающий на вход путь к файлу и подсчитывающий количество верно указанных файлов

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,27 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите первое число:");
-        int firtstNumber = new Scanner(System.in).nextInt();
-        System.out.println("Введите второе число:");
-        int secondNumber = new Scanner(System.in).nextInt();
-        System.out.println("Сумма: " + (firtstNumber + secondNumber));
-        System.out.println("Разность: " + (firtstNumber - secondNumber));
-        System.out.println("Произведение: " + (firtstNumber * secondNumber));
-        System.out.println("Частное: " + ((double) firtstNumber / secondNumber));
+        int count = 0;
+        while (true){
+            System.out.print("Введите путь к файлу: ");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if(fileExists && isDirectory){
+                System.out.println("Указан путь к папке, а не к файлу");
+                continue;
+            }
+            if(fileExists){
+                count++;
+                System.out.println("Это файл номер " + count);
+                continue;
+            }
+            if(!fileExists){
+                System.out.println("По указанному пути ничего не найдено. Попробуйте еще раз.");
+            }
+        }
     }
 }
