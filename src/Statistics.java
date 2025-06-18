8159b4606900f74fa44477df6055d5a78f288269
ASButsko <ASButsko@inno.tech>
2025-06-18 22:13:57 +0300

    В классе Statistics добавлены метод, возвращающий несуществующие страницы и метод, возвращающий статистику по браузерам

--- src/Statistics.java
+++ src/Statistics.java
@@ -9,13 +9,17 @@ public class Statistics {
     private LocalDateTime min_time;
     private LocalDateTime max_time;
     private HashSet<String> existPages;
+    private HashSet<String> unExistPages;
     HashMap<String,Integer> osCount;
+    HashMap<String,Integer> browserCount;
     public Statistics(){
         this.totalTraffic = 0;
         this.max_time = LocalDateTime.MIN;
         this.min_time = LocalDateTime.MAX;
         this.existPages = new HashSet<>();
         this.osCount = new HashMap<>();
+        this.unExistPages = new HashSet<>();
+        this.browserCount = new HashMap<>();
     }
 
 
@@ -28,13 +32,22 @@ public class Statistics {
             this.min_time = logEntry.getTime();
         }
         if(logEntry.getResponseCode() == 200){
-            existPages.add(logEntry.getPath());
+            this.existPages.add(logEntry.getPath());
+        } else if (logEntry.getResponseCode() == 404) {
+            this.unExistPages.add(logEntry.getPath());
         }
         if(!this.osCount.containsKey(logEntry.getUserAgent().getOs())){
             this.osCount.put(logEntry.getUserAgent().getOs(),1);
         }
         else{
             this.osCount.put(logEntry.getUserAgent().getOs(),osCount.get(logEntry.getUserAgent().getOs()) + 1);
+        }
+
+        if(!this.browserCount.containsKey(logEntry.getUserAgent().getBrowser())){
+            this.browserCount.put(logEntry.getUserAgent().getBrowser(),1);
+        }
+        else{
+            this.browserCount.put(logEntry.getUserAgent().getBrowser(),browserCount.get(logEntry.getUserAgent().getBrowser()) + 1);
         }
     }
 
@@ -53,6 +66,10 @@ public class Statistics {
         return this.existPages;
     }
 
+    public HashSet<String> getUnExistingPages(){
+        return this.unExistPages;
+    }
+
     public HashMap<String, Double> getOsStat(){
         double allOs = 0;
         for(int i: this.osCount.values()){
@@ -61,6 +78,18 @@ public class Statistics {
         HashMap<String, Double> stat = new HashMap<>();
         for(Map.Entry<String,Integer> entry:this.osCount.entrySet()){
             stat.put(entry.getKey(),entry.getValue()/allOs);
+        }
+        return stat;
+    }
+
+    public HashMap<String, Double> getBrowserStat(){
+        double allBrowsers = 0;
+        for(int i: this.browserCount.values()){
+            allBrowsers += i;
+        }
+        HashMap<String, Double> stat = new HashMap<>();
+        for(Map.Entry<String,Integer> entry:this.browserCount.entrySet()){
+            stat.put(entry.getKey(),entry.getValue()/allBrowsers);
         }
         return stat;
     }
